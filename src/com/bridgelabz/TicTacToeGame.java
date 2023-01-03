5ac238f60bdb5f7ba3eb197d5a9a29595ae46c6a
pranalikubade <pranalikubade2000@gmail.com>
2023-01-03 19:20:08 +0530

    added:TicTacToe_Chooseletter

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -4,6 +4,8 @@ package com.bridgelabz;
 
     public class TicTacToeGame {
         static char[] board = new char[10];
+         static char playerletter;
+         static char computerletter;
         static  void emptyboard() {
             for (int i = 1; i < board.length; i++) {
                 {
@@ -12,9 +14,21 @@ package com.bridgelabz;
             }
 
         }
+        static void  chooseletter(){
+            Scanner scan =new Scanner(System.in);
+            System.out.println("Choose a letter: X or O");
+            playerletter=scan.next().toUpperCase().charAt(0);
+            if (playerletter=='X'){
+                computerletter='O';
+            }else {
+                computerletter='X';
+            }
+        }
+
 
         public static void main(String[] args) {
             emptyboard();
+            chooseletter();
         }
     }
 
